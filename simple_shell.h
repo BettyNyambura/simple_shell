64bc27a287be7fa0857387ed6653b269d06ca616
BettyNyambura <btynyambura20@gmail.com>
2024-08-28 09:32:32 -0700

    editting header file

--- simple_shell.h
+++ simple_shell.h
@@ -21,7 +21,9 @@ char *strDup(char *str);
 char *strCpy(char *dest, char *src);
 char *strCat(char *str2, char *str1);
 int _toka(char **args);
-void shell_executor(char *buffer);
+void shell_executor(char *buffer, char **args);
 void signalHandler(int signalnum __attribute__((unused)));
+void parse_arguments(buffer, args);
+void execute_command(char *path, char **args);
 
 #endif
