c1d65026715e5055a39ff66c6b15e587ba2097d0
BettyNyambura <btynyambura20@gmail.com>
2024-08-28 09:25:41 -0700

    seperating executions into executor and utils

---
+++ shell_executor.c
@@ -0,0 +1,71 @@
+#include "simple_shell.h"
+
+/**
+ * execute_command - executes command
+ * @path: path
+ * @args: arguments
+ * Return: void
+*/
+
+void execute_command(char *path, char **args)
+{
+	int x;
+
+	if (fork() != 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		x = execve(path, args, environ);
+		if (x == -1)
+		{
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+/**
+ * shell_executor - shell exec
+ * @buffer: string with commands
+ *
+ * Return: void
+*/
+
+void shell_executor(char *buffer)
+{
+	char *args[100];
+	char *path;
+
+	parse_arguments(buffer, args);
+	if (args[0] == NULL)
+		return;
+
+	if (_strCmp(args[0], "exit") == 0)
+	{
+		free(buffer);
+		exit(0);
+	}
+
+	path = pathFinder(args[0]);
+	if (path == NULL)
+	{
+		perror("./shell");
+		return;
+	}
+
+	struct stat st;
+
+	if (stat(path, &st) == 0)
+	{
+		execute_command(path, args);
+	}
+	else
+	{
+		perror("./shell");
+	}
+
+	if (isatty(0))
+		free(path);
+}
