8168f5c699b98ba7772829e0dd26b383d304fa45
Abel Sum <abelsum1330@gmail.com>
2024-08-30 15:44:11 -0700

    Corrections on the simple shell project

---
+++ string_int.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * _isInteractive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int _isInteractive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_string_int - converts a string to an integer
+ *@str: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _string_int(char *str)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  str[i] != '\0' && flag != 2; i++)
+	{
+		if (str[i] == '-')
+			sign *= -1;
+
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (str[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
